d2c1397894d95e25c169a87de24588d19115b5b0
Simon Lundström <github-commits@soy.se>
2022-06-19 19:46:05 +0200

    user -> username to match ENV var

--- imapfilter.lua
+++ imapfilter.lua
@@ -1,10 +1,10 @@
 local server = os.getenv("SERVER")
-local user = os.getenv("USERNAME")
+local username = os.getenv("USERNAME")
 local password = os.getenv("PASSWORD")
 
-if not user or not password then
+if not server or not username or not password then
   print("SERVER, USERNAME or PASSWORD is not set")
   os.exit(1)
 end
 
-print("login to " .. server .. " as: " .. user .. ":" .. password)
+print("login to " .. server .. " as: " .. username .. ":" .. password)
